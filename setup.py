9c416be55ec8c555325cf556f9014a21026f4cc3
Björn Höfling <bjoern.hoefling@bjoernhoefling.de>
2022-10-23 07:52:59 +0200

    setup.py: Fix install_requires.

--- setup.py
+++ setup.py
@@ -13,14 +13,14 @@ setup(
     long_description_content_type='text/markdown',
     py_modules=['telepathy'],
     install_requires=[
-        'click', 'requires == 7.1.2',
-        'telethon', 'requires == 1.24.0',
-        'pandas', 'requires == 1.4.2',
-        'colorama', 'requires == 0.4.3',
-        'alive_progress', 'requires == 2.4.1',
-        'beautifulsoup4', 'requires == 4.9.3',
-        'requests', 'requires == 2.28.1',
-        'pprintpp', 'requires == 0.4.0'
+        'click ~= 7.1.2',
+        'telethon == 1.25.2',
+        'pandas == 1.4.2',
+        'colorama ~= 0.4.3',
+        'alive_progress == 2.4.1',
+        'beautifulsoup4 == 4.11.1',
+        'requests ~= 2.28.1',
+        'pprintpp == 0.4.0',
     ],
     entry_points='''
         [console_scripts]
